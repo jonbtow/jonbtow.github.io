1c913393354d88b36c3f8e148f7f9de8298e4133
jonbtow <jonathantow8@gmail.com>
2017-04-22 15:33:43 -0400

    simple update

---
+++ pushSite.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import os
+os.chdir("/Users/drseuss/Programming/MySites/Personal_Page/")
+os.system("git add *")
+os.system("git commit -m \"simple update\"")
+os.system("git push")
+
+
