761eed21e3c66597d8e579ee667b2e22915fe844
jonbtow <jonathantow8@gmail.com>
2017-05-24 00:54:02 -0400

    clean

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-# jonbtow.github.io
-My Personal Web Page.
--- _site/pushSite.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import os
-os.chdir("/Users/drseuss/Programming/MySites/Personal_Page/")
-os.system("git add *")
-os.system("git commit -m \"simple update\"")
-os.system("git push")
-
-
